09cdfff2b4d850b87be54da06e7d66b18c528db3
whitehat2k9@gmail.com <whitehat2k9@gmail.com@55bd3d29-3257-e9eb-7ca7-bcf3f0812ce4>
2011-06-30 18:39:21 +0000

    Added gaussian blur filter to petpeeve, improvements to histogram.py, added pt_remove_ext.py

--- trunk/histogram.py
+++ trunk/histogram.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
+# Kevin Zhang
+# Computes histogram and basic statistics about lesion and normal region pixel values
 
 from numpy import *
 import matplotlib.pyplot as plt
 import os, sys, dicom
 
-if len(sys.argv) != 3:
-    print "Usage: %s DCM_input_directory DCM_binmask_input_directory lesion_output_filename.png normal_output_filename.png" % (sys.argv[0])
+if len(sys.argv) != 6:
+    print "Usage: %s DCM_input_dir DCM_lesion_mask_dir DCM_body_mask_dir lesion_output.png normal_output.png" % (sys.argv[0])
     sys.exit(-1)
 
 lesion_pixvals = []
@@ -13,25 +15,27 @@ normal_pixvals = []
 
 files = os.listdir(sys.argv[1])
 for f in files:
-    inputfn = os.path.join(sys.argv[1], f)
-    maskfn = os.path.join(sys.argv[2], f)
+    inputfn = os.path.join(sys.argv[1], f) # original input DCM file
+    lesionmaskfn = os.path.join(sys.argv[2], f) # lesion mask file
+    bodymaskfn = os.path.join(sys.argv[3], f) # body mask file
     inputdcm = dicom.read_file(inputfn)
-    maskdcm = dicom.read_file(maskfn)
+    lesionmaskdcm = dicom.read_file(lesionmaskfn)
+    bodymaskdcm = dicom.read_file(bodymaskfn)
 
     input_arr = inputdcm.pixel_array
-    mask_arr = maskdcm.pixel_array
+    lesionmask_arr = lesionmaskdcm.pixel_array
+    bodymask_arr = bodymaskdcm.pixel_array
 
     rows = input_arr.shape[0]
     cols = input_arr.shape[1]
-    if rows != mask_arr.shape[0] or rows != mask_arr.shape[1]:
+    if rows != lesionmask_arr.shape[0] or rows != lesionmask_arr.shape[1]:
         print "Skipping %s, dimensions of input image and mask do not match" % (f)
         continue
 
     for r in range(0, rows):
         for c in range(0, cols):
-            if mask_arr[r][c] != 0: lesion_pixvals.append(input_arr[r][c])
-            elif input_arr[r][c] > 800: normal_pixvals.append(input_arr[r][c])
-            #else: normal_pixvals.append(input_arr[r][c]) 
+            if lesionmask_arr[r][c] != 0: lesion_pixvals.append(input_arr[r][c]) # is pixel part of lesion?
+            elif bodymask_arr[r][c] == 0: normal_pixvals.append(input_arr[r][c]) # is pixel part of body?
     print "Processed %s" % (f)
 
 print ""
@@ -41,7 +45,7 @@ n1, bins1, patches1 = plt.hist(lesion_pixvals, bins=10, facecolor='green')
 plt.xlabel("Pixel intensity")
 plt.ylabel("# of pixels")
 plt.title("Lesion")
-plt.savefig("lesion.png")
+plt.savefig(sys.argv[4])
 
 print "Lesion - min: %s" % (min(lesion_pixvals))
 print "Lesion - max: %s" % (max(lesion_pixvals))
@@ -55,7 +59,7 @@ n1, bins1, patches1 = plt.hist(normal_pixvals, bins=10, range=[0,max(lesion_pixv
 plt.xlabel("Pixel intensity")
 plt.ylabel("# of pixels")
 plt.title("Normal")
-plt.savefig("normal.png")
+plt.savefig(sys.argv[5])
 print "Normal - min: %s" % (min(normal_pixvals))
 print "Normal - max: %s" % (max(normal_pixvals))
 print "Normal - mean: %s" % (mean(normal_pixvals))
