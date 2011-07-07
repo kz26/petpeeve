60ad2e57a048cbd2d31fe698ec5f42e752d3d521
whitehat2k9@gmail.com <whitehat2k9@gmail.com@55bd3d29-3257-e9eb-7ca7-bcf3f0812ce4>
2011-07-07 21:32:38 +0000

    Further refinements(?) to petpeeve

--- trunk/kevin/clusterpoints.py
+++ trunk/kevin/clusterpoints.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 import scipy as sp
 from scipy.cluster.vq import whiten, kmeans2
+from scipy.cluster.hierarchy import linkage
 
 if len(sys.argv) != 2:
     print "Usage: %s input_data_file" % (sys.argv[0])
@@ -14,7 +15,8 @@ if len(sys.argv) != 2:
 data = np.genfromtxt(sys.argv[1])
 #data = whiten(data)
 
-clusters = kmeans2(data, 10, 100)
+clusters = kmeans2(data, 20, minit='points')
+#clusters = linkage(data)
 
-print clusters[0]
+print clusters
 
