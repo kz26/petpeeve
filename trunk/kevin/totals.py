e37fd7b65df52a59852353f7b99311858b57a3e1
whitehat2k9@gmail.com <whitehat2k9@gmail.com@55bd3d29-3257-e9eb-7ca7-bcf3f0812ce4>
2011-07-28 03:16:46 +0000

    Fixed bugs in totals.py

--- trunk/kevin/totals.py
+++ trunk/kevin/totals.py
@@ -14,18 +14,17 @@ if len(sys.argv) != 2:
 #sens_pat = re.compile("Sensitivity: ([0-9]+)")
 #tp_pat = re.compile("TP: ([0-9]+)")
 #fp_pat = re.compile("FP: ([0-9]+)")
-linepat = re.compile("[A-Za-z]: [0-9]+")
 
 f = open(sys.argv[1], 'r')
 lines = []
 for line in f:
-    if not linepat.match(line.rstrip()): continue
-    lines.append(int(line.rstrip().split(" ")[1]))
+    lines.append(line.rstrip())
+f.close()
 
-sens = [lines[i] for i in range(1, len(lines), 4)]
-tp = [lines[i] for i in range(2, len(lines), 4)]
-fp = [lines[i] for i in range(3, len(lines), 4)]
+sens = [float(lines[i].split(" ")[1]) for i in range(1, len(lines), 4)]
+tp = [int(lines[i].split(" ")[1]) for i in range(2, len(lines), 4)]
+fp = [int(lines[i].split(" ")[1]) for i in range(3, len(lines), 4)]
 
-print "Total sensitivity: %s" (np.mean(sens))
-print "Total TPs: %s" % (len(tp))
-print "Total FPs: %s" % (len(fp))
+print "Total sensitivity: %s" % (np.mean(sens))
+print "Total TPs: %s" % (sum(tp))
+print "Total FPs: %s" % (sum(fp))
